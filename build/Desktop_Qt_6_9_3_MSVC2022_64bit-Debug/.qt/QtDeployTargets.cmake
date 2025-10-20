7f500ff850de47a965bd5bcca51a36dd9a086631
jianzhangg <jianzhangg@qq.com>
2025-10-20 20:38:46 +0800

    dldl-lhsj

---
+++ build/Desktop_Qt_6_9_3_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_dldl-lhsj_FILE C:/code/dldl-lhsj/build/Desktop_Qt_6_9_3_MSVC2022_64bit-Debug/dldl-lhsj.exe)
+set(__QT_DEPLOY_TARGET_dldl-lhsj_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_dldl-lhsj_RUNTIME_DLLS D:/Qt/6.9.3/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Qt/6.9.3/msvc2022_64/bin/Qt6Guid.dll;D:/Qt/6.9.3/msvc2022_64/bin/Qt6Cored.dll)
---
+++ build/Desktop_Qt_6_9_3_MSVC2022_64bit-Debug/vcpkg-dependencies/toolchain.cmake
@@ -0,0 +1,7 @@
+
+        set(CMAKE_C_COMPILER "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/bin/HostX64/x64/cl.exe")
+        set(CMAKE_CXX_COMPILER "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/bin/HostX64/x64/cl.exe")
+        
+        set(VCPKG_TARGET_TRIPLET x64-windows)
+        include("C:/Program Files/Microsoft Visual Studio/2022/Community/VC/vcpkg/scripts/buildsystems/vcpkg.cmake")
+      
